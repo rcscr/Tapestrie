8b4014ccdd6e39b965a93b4c17d6286ca2b378b0
Raphael Correa <julio123dog@gmail.com>
2024-05-23 20:48:42 -0300

    make TrieNode into a data class

--- src/main/kotlin/com/rcs/trie/TrieNode.kt
+++ src/main/kotlin/com/rcs/trie/TrieNode.kt
@@ -1,6 +1,6 @@
 package com.rcs.trie
 
-class Node<T>(val string: String, val value: T?, val next: MutableSet<Node<T>>) {
+data class Node<T>(val string: String, val value: T?, val next: MutableSet<Node<T>>) {
     fun completes(): Boolean {
         return value != null
     }
