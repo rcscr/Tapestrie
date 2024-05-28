4d5dcce28fa44eb606b7b19da712997e1fa71517
Raphael Correa <julio123dog@gmail.com>
2024-05-28 00:03:05 -0300

    improve comment structure

--- src/main/kotlin/com/rcs/trie/FuzzySubstringMatchingStrategy.kt
+++ src/main/kotlin/com/rcs/trie/FuzzySubstringMatchingStrategy.kt
@@ -1,7 +1,12 @@
 package com.rcs.trie
 
 enum class FuzzySubstringMatchingStrategy {
-    LIBERAL, // matches everywhere in the string
-    MATCH_PREFIX, // matches only words that start with the first letter of the keyword
-    ANCHOR_TO_PREFIX, // similar to MATCH_PREFIX, but allows applying error tolerance in the beginning
+    // matches everywhere in the string
+    LIBERAL,
+
+    // matches only words that start with the first letter of the keyword
+    MATCH_PREFIX,
+
+    // similar to MATCH_PREFIX, but allows applying error tolerance in the beginning
+    ANCHOR_TO_PREFIX,
 }
