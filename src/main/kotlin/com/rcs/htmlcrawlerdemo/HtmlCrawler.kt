62cbe2f0b40959a44b086b7bc02145324f0ee51d
Raphael Correa <julio123dog@gmail.com>
2024-06-16 09:40:05 -0300

    move all url logic to HtmlUrlFinder and fix some edge cases

--- src/main/kotlin/com/rcs/htmlcrawlerdemo/HtmlCrawler.kt
+++ src/main/kotlin/com/rcs/htmlcrawlerdemo/HtmlCrawler.kt
@@ -19,7 +19,7 @@ class HtmlCrawler(
 
         val currentTimeMillis = System.currentTimeMillis()
         val trie: Trie<ConcurrentLinkedDeque<HtmlIndexEntry>> = Trie()
-        val pagesIndexed = crawl("", ConcurrentHashMap(), trie)
+        val pagesIndexed = crawl(baseUrl, ConcurrentHashMap(), trie)
         val durationMillis = System.currentTimeMillis() - currentTimeMillis
 
         println("Done initializing crawler; " +
@@ -30,54 +30,45 @@ class HtmlCrawler(
     }
 
     private fun crawl(
-        relativeUrl: String,
+        url: String,
         visited: ConcurrentHashMap<String, Boolean?>,
         trie: Trie<ConcurrentLinkedDeque<HtmlIndexEntry>>
     ): Int {
         // Use putIfAbsent to check and mark the URL atomically
-        if (visited.putIfAbsent(relativeUrl, true) != null) {
+        if (visited.putIfAbsent(url, true) != null) {
             return 0 // URL already visited
         }
 
         val htmlContent: String
         try {
-            htmlContent = htmlClient.getAsString(baseUrl + relativeUrl)
+            htmlContent = htmlClient.getAsString(url)
         } catch (e: IOException) {
             e.printStackTrace()
-            println("Error fetching ${baseUrl + relativeUrl} - not indexing page")
+            println("Error fetching $url - not indexing page")
             return 0
         }
 
-        indexPage(relativeUrl, htmlContent, trie)
+        indexPage(url, htmlContent, trie)
 
-        val newCounts = htmlUrlFinder.findRelativeUrls(htmlContent)
-            .map { u -> fixUrl("/$relativeUrl", u) }
+        val newCounts = htmlUrlFinder.findRelativeUrls(url, htmlContent)
             .map { u -> executorService.submit<Int> { crawl(u, visited, trie) } }
             .sumOf { it.get() }
 
         return 1 + newCounts
     }
 
-    private fun fixUrl(relativeUrl: String, additionalPath: String): String {
-        var fixedUrl = additionalPath
-        var prefixUrl = relativeUrl.substring(0, relativeUrl.lastIndexOf("/"))
-        while (fixedUrl.startsWith("../")) {
-            fixedUrl = fixedUrl.replace("../", "")
-            prefixUrl = prefixUrl.substring(0, prefixUrl.lastIndexOf("/"))
-        }
-        return prefixUrl + fixedUrl
-    }
+    private fun indexPage(url: String, htmlContent: String, trie: Trie<ConcurrentLinkedDeque<HtmlIndexEntry>>) {
+        println("Indexing $url")
 
-    private fun indexPage(relativeUrl: String, htmlContent: String, trie: Trie<ConcurrentLinkedDeque<HtmlIndexEntry>>) {
-        println("Indexing ${baseUrl + relativeUrl}")
+        // stores only relative URLs in order to minimize storage space
+        // the full URL must then be reconstructed on retrieval!
+        val relativeUrl = url.substring(0, url.length)
 
         htmlTokenizer.tokenize(htmlContent)
             .forEach { entry ->
                 val token = entry.key
                 val occurrences = entry.value
                 val indexEntries = trie.getExactly(token) ?: ConcurrentLinkedDeque()
-                // stores only relative URLs in order to minimize storage space
-                // the full URL must then be reconstructed on retrieval!
                 val newEntry = HtmlIndexEntry(relativeUrl, occurrences)
                 indexEntries.add(newEntry)
                 indexEntries.sortedBy { it.occurrences }
--- src/main/kotlin/com/rcs/htmlcrawlerdemo/HtmlUrlFinder.kt
+++ src/main/kotlin/com/rcs/htmlcrawlerdemo/HtmlUrlFinder.kt
@@ -6,20 +6,54 @@ import java.util.regex.Pattern
 class HtmlUrlFinder {
 
     companion object {
-        private val relativeHrefRegex =
+        private const val relativeHrefRegex =
             "<a\\s+[^>]*href\\s*=\\s*\"(?!http|https|mailto|ftp)([^\"]*\\.html)\""
 
         private val relativeHrefPattern =
             Pattern.compile(relativeHrefRegex, Pattern.CASE_INSENSITIVE)
     }
 
-    fun findRelativeUrls(htmlContent: String): Set<String> {
+    fun findRelativeUrls(url: String, htmlContent: String): Set<String> {
         val matcher = relativeHrefPattern.matcher(htmlContent)
         val links: MutableSet<String> = HashSet()
         while (matcher.find()) {
             val link = matcher.group(1)
             links.add(link)
         }
-        return links
+        return links.map { makeFullUrl(url, it) }.toSet()
+    }
+
+    private fun makeFullUrl(baseUrl: String, additionalPath: String): String {
+        var fixedBaseUrl = baseUrl
+        var fixedAdditionalUrl = additionalPath
+
+        // remove any file name from base URL
+        fixedBaseUrl = fixedBaseUrl.substring(0, fixedBaseUrl.lastIndexOf("/"))
+
+        // remove any possible first / for consistency
+        if (fixedAdditionalUrl.startsWith("/")) {
+            fixedAdditionalUrl = fixedAdditionalUrl.replaceFirst("/", "")
+        }
+
+        // file in same directory
+        if (fixedAdditionalUrl.startsWith("./")) {
+            fixedAdditionalUrl = fixedAdditionalUrl.replaceFirst("./", "")
+        }
+
+        // if additionalUrl contains a ../ in the middle,
+        // remove it and make the appropriate changes in the same variable
+        val index = fixedAdditionalUrl.indexOf("../")
+        if (index > 1) {
+            fixedAdditionalUrl = fixedAdditionalUrl.substring(index + 3, fixedAdditionalUrl.length)
+        }
+
+        // if additionalUrl contains a ../ in the start,
+        // remove it and make the appropriate changes in fixedBaseUrl
+        while (fixedAdditionalUrl.startsWith("../")) {
+            fixedAdditionalUrl = fixedAdditionalUrl.replace("../", "")
+            fixedBaseUrl = fixedBaseUrl.substring(0, fixedBaseUrl.lastIndexOf("/"))
+        }
+
+        return ("$fixedBaseUrl/$fixedAdditionalUrl")
     }
 }
